d6f841efa5ef3ceffdb23507d315496d9a74edb6
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-21 08:58:23 -0700

    pins controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pins/index'
+
   get 'pinboard/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pinboard#index'
+
+  resources 'pins'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
